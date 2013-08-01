fa5b879330778594fb5e93db176197469220e61a
Robert J Martin <robertmartinbc@shaw.ca>
2013-08-01 09:58:18 -0700

    First commit and README update.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CupidiconProduct::Application.config.secret_token = '14f2cde5af66cb421eb6b12076d01cbd7441f9cf1b4ac2e48087542c31af05d8a2aa362f8317a853502445699461950f5628ba19270136a1695a98cfc2eb63fe'
