11e614497928083c8c622bba20561b7b92478b1a
Robert J Martin <robertmartinbc@shaw.ca>
2013-08-01 10:40:01 -0700

    Created comment models with relationships.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,3 +1,4 @@
 class Assignment < ActiveRecord::Base
+  has_many :comments
   attr_accessible :body, :category, :cost, :instructions, :key_words, :language, :rating, :style, :title, :word_count
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130801173055) do
+ActiveRecord::Schema.define(:version => 20130801173614) do
 
   create_table "assignments", :force => true do |t|
     t.string   "title"
@@ -27,5 +27,14 @@ ActiveRecord::Schema.define(:version => 20130801173055) do
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
   end
+
+  create_table "comments", :force => true do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "comments", ["post_id"], :name => "index_comments_on_post_id"
 
 end
