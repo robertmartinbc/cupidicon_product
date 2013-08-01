9475407ed2511018e1b7063a36adafdff4afbba9
Robert J Martin <robertmartinbc@shaw.ca>
2013-08-01 11:45:18 -0700

    Added create and update CRUD actions.

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -8,8 +8,32 @@ class AssignmentsController < ApplicationController
   end
 
   def new
+    @assignment = Assignment.new
   end
 
+  def create
+  @assignment = Assignment.new(params[:assignment])
+  if @assignment.save
+    flash[:notice] = "Assignment was saved."
+    redirect_to @assignment
+  else
+    flash[:error] = "There was an error saving the assignment. Please try again."
+    render :new
+  end
+end
+
   def edit
+    @assignment = Assignment.find(params[:id])
+  end
+
+  def update
+    @assignment = Assignment.find(params[:id])
+    if @assignment.update_attributes(params[:post])
+      flash[:notice] = "Assignment was updated."
+      redirect_to @assignment
+    else
+      flash[:error] = "There was an error saving the assignment. Please try again."
+      render :new
+    end
   end
 end
