12d869edb1df72a1e39ed187d223a66741ea7fd2
Robert J Martin <robertmartinbc@shaw.ca>
2013-08-01 15:32:34 -0700

    Added validation methods to assignment, refactored assignment views to use a form partial.

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -4,4 +4,8 @@ class Assignment < ActiveRecord::Base
   attr_accessible :body, :category, :cost, :instructions, :key_words, :language, :rating, :style, :title, :word_count
 
   default_scope order('created_at DESC')
+
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true  
+  validates :user, presence: true  
 end
